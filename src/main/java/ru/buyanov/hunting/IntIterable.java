e7c272c900681fdeb122089cc4fabcd1335c7c9b
Vasyunin Alex <efg.efg.0001@gmail.com>
2016-07-20 16:18:45 +0300

    Added implementation of integer iterator

--- src/main/java/ru/buyanov/hunting/IntIterable.java
+++ src/main/java/ru/buyanov/hunting/IntIterable.java
@@ -18,15 +18,14 @@ public class IntIterable implements Iterable<Integer> {
     }
 
     private class IntIterator implements Iterator<Integer> {
+        private int currentIndex = 0;
 
         public boolean hasNext() {
-            //TODO: You task is implement this method
-            return false;
+            return currentIndex < backed.length;
         }
 
         public Integer next() {
-            //TODO: You task is implement this method
-            return null;
+            return backed[currentIndex++];
         }
 
         public void remove() {

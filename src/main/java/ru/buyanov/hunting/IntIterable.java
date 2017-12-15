fd01e0f6c8132db5e69e8e7931207d1609c0419a
Пышка Пшеничная < >
2017-12-15 15:08:05 +0300

    added new benchmarks

--- src/main/java/ru/buyanov/hunting/IntIterable.java
+++ src/main/java/ru/buyanov/hunting/IntIterable.java
@@ -7,6 +7,7 @@ import java.util.Iterator;
  */
 public class IntIterable implements Iterable<Integer> {
     int[] backed;
+    int numId = 0;
 
 
     public IntIterable(int[] backed) {
@@ -20,13 +21,11 @@ public class IntIterable implements Iterable<Integer> {
     private class IntIterator implements Iterator<Integer> {
 
         public boolean hasNext() {
-            //TODO: You task is implement this method
-            return false;
+            return numId<backed.length;
         }
 
         public Integer next() {
-            //TODO: You task is implement this method
-            return null;
+            return backed[numId++];
         }
 
         public void remove() {
